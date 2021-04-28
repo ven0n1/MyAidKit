e0d12de098089bf72dfe3fdfad291e14c8537891
ven0n1 <dminyof1@mail.ru>
2021-04-28 20:23:43 +0300

    main

---
+++ app/src/main/java/com/example/myaidkit/Reminder.java
@@ -0,0 +1,26 @@
+package com.example.myaidkit;
+
+import java.util.Calendar;
+
+public class Reminder {
+    String name;
+    String date;
+    String time;
+
+    public Reminder(String name, String date, String time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
---
+++ app/src/main/java/com/example/myaidkit/ReminderAdapter.java
@@ -0,0 +1,37 @@
+package com.example.myaidkit;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ReminderAdapter extends ArrayAdapter <Reminder> {
+
+    public ReminderAdapter(@NonNull Context context, @NonNull Reminder[] objects) {
+        super(context, R.layout.reminder_item, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Reminder reminder = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.reminder_item, null);
+        }
+        if (position%2==0){
+            convertView.setBackgroundColor(Color.argb(150, 150, 150, 150));
+        } else {
+            convertView.setBackgroundColor(Color.argb(150, 255, 255, 255));
+        }
+        ((TextView) convertView.findViewById(R.id.name_reminder)).setText(reminder.getName());
+        ((TextView) convertView.findViewById(R.id.date_reminder)).setText(reminder.getDate());
+        ((TextView) convertView.findViewById(R.id.time_reminder)).setText(reminder.getTime());
+        return convertView;
+    }
+}
